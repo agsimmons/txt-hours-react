d2530a4d65bddec4bafb537edaf1a679711cf89c
Andrew Simmons <agsimmons0@gmail.com>
2025-08-31 18:23:04 -0400

    Add help and state system

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import { useRef, useState } from "react";
+import {
+  useRef,
+  useState,
+  useMemo,
+  type SetStateAction,
+  type Dispatch,
+} from "react";
 import { Temporal } from "temporal-polyfill";
 import "./App.css";
+
+type UIState = "input" | "help" | "error" | "result";
 
 type DayBlock = {
   date: Temporal.PlainDate;
@@ -27,52 +35,67 @@ const ANCHOR_DATE = new Temporal.PlainDate(2000, 1, 1);
 
 type ResultTableProps = {
   validatedData: ValidatedData;
+  onReturnToInput: () => void;
 };
 
-function ResultTable({ validatedData }: ResultTableProps) {
+function ResultTable({ validatedData, onReturnToInput }: ResultTableProps) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th scope="col"></th>
-          {validatedData.dates.map((date) => (
-            <th key={date.toString()} scope="col">
-              {date.toString()}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {validatedData.taskNames.map((taskName) => (
-          <tr key={taskName}>
-            <th scope="row">{taskName}</th>
+    <>
+      <button type="button" onClick={onReturnToInput}>
+        Return to input
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col"></th>
             {validatedData.dates.map((date) => (
-              <td key={date.toString()}>
-                {validatedData.taskIndexedTaskDurations
-                  .get(taskName)
-                  ?.get(date) !== undefined
-                  ? Number(
-                      validatedData.taskIndexedTaskDurations
-                        .get(taskName)
-                        ?.get(date)
-                        ?.total({ unit: "hours" })
-                    ).toFixed(2)
-                  : ""}
-              </td>
+              <th key={date.toString()} scope="col">
+                {date.toString()}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {validatedData.taskNames.map((taskName) => (
+            <tr key={taskName}>
+              <th scope="row">{taskName}</th>
+              {validatedData.dates.map((date) => (
+                <td key={date.toString()}>
+                  {validatedData.taskIndexedTaskDurations
+                    .get(taskName)
+                    ?.get(date) !== undefined
+                    ? Number(
+                        validatedData.taskIndexedTaskDurations
+                          .get(taskName)
+                          ?.get(date)
+                          ?.total({ unit: "hours" })
+                      ).toFixed(2)
+                    : ""}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
-function App() {
-  const textInputRef = useRef<HTMLTextAreaElement | null>(null);
+type ErrorDisplayProps = {
+  message: string;
+};
 
-  const [validatedData, setValidatedData] = useState<ValidatedData | null>(
-    null
-  );
+function ErrorDisplay({ message }: ErrorDisplayProps) {
+  return <p>{message}</p>;
+}
+
+type DataEntryProps = {
+  setValidatedData: Dispatch<SetStateAction<ValidatedData | null>>;
+  setUIState: Dispatch<SetStateAction<UIState>>;
+};
+
+function DataEntry({ setValidatedData, setUIState }: DataEntryProps) {
+  const textInputRef = useRef<HTMLTextAreaElement | null>(null);
 
   const processData = () => {
     setValidatedData(null);
@@ -176,16 +199,152 @@ function App() {
       ),
       taskIndexedTaskDurations: taskIndexedTaskDurations,
     });
+    setUIState("result");
+
+    // TODO: Handle errors
   };
 
   return (
     <>
-      <h1>txt-hours</h1>
       <textarea ref={textInputRef} style={{ width: "100%", height: "75vh" }} />
       <button type="button" onClick={processData}>
         Submit
       </button>
-      {validatedData && <ResultTable validatedData={validatedData} />}
+    </>
+  );
+}
+
+type HelpGuideProps = {
+  onReturnToInput: () => void;
+};
+
+function HelpGuide({ onReturnToInput }: HelpGuideProps) {
+  return (
+    <>
+      <h2>Help</h2>
+      <button type="button" onClick={onReturnToInput}>
+        Return to input
+      </button>
+      <p>
+        txt-hours is a method for tracking your time spent on tasks throughout
+        the day. The start and stop times of each task is recorded in a
+        well-defined plaintext format. This text can then be parsed to display a
+        table of the total amount of time in hours spent on each task each day.
+      </p>
+
+      <p>
+        Your txt-hours document is composed of a list of day blocks separated by
+        an empty line. Each day block starts with the day's date in YYYY-MM-DD
+        format on it's own line, followed by one or more task record. Each task
+        record is composed of a start time, an end time, and a task description.
+        Each time is entered in HH:MM format, where hour is between 1 and 12
+        inclusive. AM and PM is inferred automatically and must not be written
+        out.
+      </p>
+
+      <p>
+        To use this method, create a new text document on your computer when you
+        are ready to begin working on tasks. Begin the document with today's
+        date, and start your first task record with the current time. As you
+        don't yet know when you will switch tasks, enter ?? as the end time for
+        the record. Enter in a task description, and begin your work. When you
+        are ready to switch tasks, replace the ?? placeholder for the end task,
+        and enter in the actual end time. Start a new task record on the next
+        line and begin your next task. At the end of the day, you will have a
+        complete record of your day's work. Repeat this process the next day
+        with a new day block.
+      </p>
+
+      <p>
+        Aim to keep your task descriptions general and re-usable. Instead of
+        saying "Fix typo in intro paragragh of my book", say "Novel Writing".
+        This will allow you to get totals for types of work in your
+        summaziation.
+      </p>
+
+      <p>
+        When you would like to summarize your time, visit this site and paste
+        the contents of your text document into the text input area. You would
+        typically only copy data that you have not yet summarized, but this
+        would vary by workflow. Press Submit, and you will be presented with the
+        result table. Columns are shown for each day recorded in your text
+        document sorted by date. Rows are shown for each unique task description
+        sorted alphabetically. The cells at the intersection of the two contain
+        the number of hours spent on that task on that day, rounded to two
+        decimal places.
+      </p>
+
+      <p>
+        If pressing the submit button doesn't do anything, or if you are shown
+        an error message, then there is an error in your text. Some common
+        mistakes are that you forgot to replace a ?? placeholder time, you
+        missed a : in one of your times, you missed the - between times, or that
+        you missed the : before your task description. Look over your document
+        for any mistakes, correct them, and try submitting again. If you still
+        have an error, try removing the last few days from the text and submit
+        again. This can help you narrow down where the error is. I would like to
+        add better error handling so that the exact location of your error can
+        be pointed out to you, but that has not yet been implemented.
+      </p>
+
+      <p>
+        If you encounter any bugs, or would like to make a feature suggestion,
+        create an Issue on the repo here:
+        <a
+          href="https://github.com/agsimmons/txt-hours-react"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          https://github.com/agsimmons/txt-hours-react
+        </a>
+      </p>
+    </>
+  );
+}
+
+function App() {
+  const [uiState, setUIState] = useState<UIState>("input");
+
+  const [validatedData, setValidatedData] = useState<ValidatedData | null>(
+    null
+  );
+
+  const pageContents = useMemo((): React.JSX.Element | null => {
+    switch (uiState) {
+      case "input":
+        return (
+          <DataEntry
+            setValidatedData={setValidatedData}
+            setUIState={setUIState}
+          />
+        );
+      case "help":
+        return <HelpGuide onReturnToInput={() => setUIState("input")} />;
+      case "error":
+        return <p>TODO: Error State</p>;
+      case "result":
+        return (
+          validatedData && (
+            <ResultTable
+              validatedData={validatedData}
+              onReturnToInput={() => setUIState("input")}
+            />
+          )
+        );
+      default:
+        return null;
+    }
+  }, [uiState, validatedData]);
+
+  return (
+    <>
+      <h1>txt-hours</h1>
+      {uiState === "input" && (
+        <button type="button" onClick={() => setUIState("help")}>
+          Help
+        </button>
+      )}
+      {pageContents}
     </>
   );
 }
